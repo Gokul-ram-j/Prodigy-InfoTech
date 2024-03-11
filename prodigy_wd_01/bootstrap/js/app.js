2917e59bf75692fe728e46aa384c73f90d4a9701
gokul-ram-j <gokulram221@gmail.com>
2024-03-11 20:47:33 +0530

    11-03

---
+++ prodigy_wd_01/bootstrap/js/app.js
@@ -0,0 +1,79 @@
+
+let slide=[
+"https://getwallpapers.com/wallpaper/full/2/1/e/681500.jpg",
+"https://wallpapercave.com/wp/wp1811272.jpg",
+"https://wallpapercave.com/wp/wp3770517.png",
+"https://getwallpapers.com/wallpaper/full/b/0/9/14123.jpg",
+"https://wallup.net/wp-content/uploads/2016/05/27/328236-Grand_Theft_Auto_V.jpg",
+"https://wallpapercave.com/wp/wp8172984.jpg",
+"https://hdqwalls.com/wallpapers/spiderman-2018-game-4k-0h.jpg"
+]
+
+let trending_games=[
+"https://cdn1.epicgames.com/offer/6e6aa039c73347b885803de65ac5d3db/EGS_GhostofTsushima_SuckerPunchProductions_S2_1200x1600-e23e02c1d70be7b528dba50860f87d39?h=480&quality=medium&resize=1&w=360",
+"https://cdn1.epicgames.com/offer/7b57136ac766477eb1e687dcd0310da5/EGS_WarMongrels_DestructiveCreations_S2_1200x1600-f9e30ca0ab7960795a9a933f4c1b8bf8?h=480&quality=medium&resize=1&w=360",
+"https://cdn1.epicgames.com/offer/611482b8586142cda48a0786eb8a127c/EGS_DeadbyDaylight_BehaviourInteractive_S2_1200x1600-a1c30209e3b9fb63144daa9b5670f503?h=480&quality=medium&resize=1&w=360",
+"https://cdn1.epicgames.com/offer/236c74b4cd2e4e3099cbe2ebdc9686fd/EGS_DeadIsland2_DeepSilverDambusterStudios_S2_1200x1600-efc5201842cf642eb45f73227cd0789b?h=480&quality=medium&resize=1&w=360",
+"https://cdn1.epicgames.com/offer/77f2b98e2cef40c8a7437518bf420e47/EGS_Cyberpunk2077_CDPROJEKTRED_S2_03_1200x1600-b1847981214ac013383111fc457eb9c5?h=480&quality=medium&resize=1&w=360",
+"https://cdn1.epicgames.com/offer/9bcf5a4dc1d54cb6ab1a42f3a70c5cf4/Carousel_BoxArt_1200x1600_1200x1600-38bda67bb1290f58e8a18ddc38a3c0ec?h=480&quality=medium&resize=1&w=360",
+"https://cdn1.epicgames.com/offer/24cc2629b0594bf29340f6acf9816af8/EGS_HorizonForbiddenWestCompleteEdition_GuerrillaGamesNixxesSoftware_S2_1200x1600-6eeadae1c58ebaaa74b109bd26d96645?h=480&quality=medium&resize=1&w=360",
+"https://cdn1.epicgames.com/6504cc61472e498796e0b4963a201438/offer/EGS_TheOutlastTrials_RedBarrels_S2-1200x1600-c70676de7925195513fa7bc4bc2f1d9b.jpg?h=480&quality=medium&resize=1&w=360"
+
+]
+let most_popular_games=[
+"https://cdn1.epicgames.com/offer/cbd5b3d310a54b12bf3fe8c41994174f/EGS_VALORANT_RiotGames_S2_1200x1600-a863fbd473de6a1ea1ff26278fb025ed?h=480&quality=medium&resize=1&w=360",
+"https://cdn1.epicgames.com/offer/4750c68b2bfa4f43933b81cfd5cc510c/EGS_EASPORTSFC24StandardEdition_EACanada_S2_1200x1600-37aa3816ed46542836e1068060998810?h=480&quality=medium&resize=1&w=360",
+"https://cdn1.epicgames.com/spt-assets/764b2d57552c436590f50318bd7587f9/download-bloons-td-6-offer-100fo.jpg?h=480&quality=medium&resize=1&w=360",
+"https://cdn1.epicgames.com/offer/2a09fb19b47f46dfb11ebd382f132a8f/EGS_MarvelsGuardiansoftheGalaxy_EidosMontral_S2_01_1200x1600-e8e868690d5e67692c7bf2c13aa84561?h=480&quality=medium&resize=1&w=360",
+"https://cdn1.epicgames.com/offer/2c42520d342a46d7a6e0cfa77b4715de/StoreVertical1200x16001_1200x1600-c3984f399a98d62bc56770577508b890?h=480&quality=medium&resize=1&w=360",
+"https://cdn1.epicgames.com/95d0b9561be1464cb43bd029e94cf526/offer/GR_Portrait_Offer_1200x1600-1200x1600-a7811e23904db375486535513d10412f.jpg?h=480&quality=medium&resize=1&w=360",
+]
+
+let top_featured_games=[
+"https://cdn1.epicgames.com/spt-assets/7a89e61918a941289b86034a42ffaf96/metal-mind-mt2i4.jpg?h=480&quality=medium&resize=1&w=360",
+"https://cdn1.epicgames.com/spt-assets/ce12d040c108495c89ccc6b2e8ebe3c4/choochoo-charles-yffjr.png?h=480&quality=medium&resize=1&w=360",
+"https://cdn1.epicgames.com/salesEvent/salesEvent/EGS_GenshinImpact_miHoYoLimited_S2_1200x1600-c12cdcc2cac330df2185aa58c508e820?h=480&quality=medium&resize=1&w=360",
+"https://cdn1.epicgames.com/spt-assets/a2a82a68f6f445c4a5179375fac55179/creatures-of-ava-c1230.jpg?h=480&quality=medium&resize=1&w=360",
+"https://cdn1.epicgames.com/spt-assets/557fb2a2e101498bb9fe6998ee36154b/bogx-109cg.jpg?h=480&quality=medium&resize=1&w=360",
+"https://cdn1.epicgames.com/spt-assets/62b3525c0dae4deebb9b66f0d17bfd3f/banishers--ghosts-of-new-eden-2uw71.jpg?h=480&quality=medium&resize=1&w=360",
+"https://cdn1.epicgames.com/offer/22600f09e936468c8ecfc22b5eac7d7c/EGST_StorePortrait_1200x1600_1200x1600-54fe39c69e335bbe2c071ff53cfa0685?h=480&quality=medium&resize=1&w=360"
+]
+
+
+function makeslide(name_list){
+    let elem="";
+    for(nme of name_list){
+        elem+=
+        `
+        <div class="swiper-slide">
+            <img src=${nme} alt="">
+            <div class="btngrp  ">
+              <button class="btn btn-primary m-1 p-2" >Purchase Now <i class="fa fa-youtube" aria-hidden="true"></i></button>
+              <button class="btn btn-primary m-1 p-2">Watch Trailer <i class="fa fa-external-link" aria-hidden="true"></i></button>
+            </div>
+          </div>
+        `
+    }
+    return elem;
+}
+
+
+
+
+function makecards(name_list){
+    let elem="";
+    for(nme of name_list){
+        elem+=
+        `
+        <div class="card">
+        <img src=${nme} alt="">
+        </div>
+        `
+    }
+    return elem
+}
+
+document.querySelector(".trending").innerHTML=makecards(trending_games)
+document.querySelector(".most-popular").innerHTML=makecards(most_popular_games)
+document.querySelector(".top-featured").innerHTML=makecards(top_featured_games)
+document.querySelector(".swiper-wrapper").innerHTML=makeslide(slide)
